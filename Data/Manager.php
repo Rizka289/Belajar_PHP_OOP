c9750fcb7c31945769d5231ca8f44b3e19067c16
Rizka <rizkaarachmawati998@gmail.com>
2022-08-30 20:05:57 +0700

    FunctionOverriding

--- Data/Manager.php
+++ Data/Manager.php
@@ -7,10 +7,15 @@ class Manager {
 
     // void -> tidak mengembalikan value 
     function sayHello(string $name):void{
-        echo "Hi $name, my name is $this->name" . PHP_EOL;
+        echo "Hi $name, my name is Manager $this->name" . PHP_EOL;
     }
 }
 
-class VicePresident extends Manager{
+class VicePresident extends Manager
+{
+    // function overriding -> menimpa dengan function barunya
+    function sayHello(string $name):void{
+        echo "Hi $name, my name is VP $this->name" . PHP_EOL;
+    }
 
 }
